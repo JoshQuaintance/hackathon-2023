1d60f6df680d789248fb11a150812f2efe8caf4d
Joshua Quaintance <45566099+JoshQuaintance@users.noreply.github.com>
2023-02-04 12:16:55 -0500

    Frontend started

---
+++ frontend/src/routes/data/[...catchall]/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import type { PageData } from '@sveltejs/kit';
+
+    export let data: PageData;
+</script>
+
+<main>
+    <h2>GS1 Tag</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>AI</th>
+                <th>Data</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">{data.all}</th>
+                <td>
+                    {data.all}
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <!-- <script src="script.js"></script> -->
+</main>
---
+++ frontend/src/routes/data/[...catchall]/+page.ts
@@ -0,0 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
+import { error } from '@sveltejs/kit';
+
+/** @type {import('@sveltejs/kit').PageLoad} */
+export function load({ params }) {
+    return {
+        header: 'x7F',
+        aidc: 'x22',
+        filter: 'b010',
+        sscc: 'xA560100F9',
+        ai: 'x02',
+        gtin: 'x0E192001AF3',
+        all: params.catchall
+
+    };
+
+}
