81b33d7004d7f9a99f6a11481fdc7d1f88402497
Joshua Quaintance <45566099+JoshQuaintance@users.noreply.github.com>
2023-02-05 12:57:14 -0500

    Last Query

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,5 +2,7 @@
     <h1>WRxIGHT Solutions</h1>
     <h6>Your wright solution to your supply chain problems</h6>
 
-    
+    <a href="/query"><p style="border: solid 2px white; width: 5rem;">Create</p></a>
+    <br />
+    <a href="/query"><p style="border: solid 2px white; width: 5rem;">Query</p></a>
 </div>
