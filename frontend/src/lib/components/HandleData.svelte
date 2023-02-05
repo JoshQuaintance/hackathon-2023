868b18467a9a8f9a9643fc35ccfd8f0224d1970a
Striker2783 <owenxingli@gmail.com>
2023-02-05 01:01:13 -0500

    Added svelte to allow for recursive showing

--- frontend/src/lib/components/HandleData.svelte
+++ frontend/src/lib/components/HandleData.svelte
@@ -1,24 +1,37 @@
 <script lang="ts">
-    import type { ProductData } from '$lib/props/types';
-
-    export let data: ProductData;
+    export let data: Object;
     const colors_array: string[] = ['#3d7475', '#479c9d', '#6eb5aa', '#88af95', '#819a78'];
 </script>
 
 {#each Object.entries(data) as [header, value]}
     <div class="item">{header}</div>
-    {#if typeof value == 'object'}
+    {#if value === null}
+        <div class="item">{''}</div>
+    {:else if typeof value == 'object'}
         <div class="item">
-            <button type="button" class="collapsible">
-                <div class="content">
-                    {#if value["data"] === undefined}
-
-                    {/if}
-                    {#each Object.entries(value) as [header2, value2]}
-                        <div class="item">{header2}</div>
-                    {/each}
-                </div>
-            </button>
+            {#if 'data' in value}
+                <button type="button" class="collapsible">
+                    <div class="content">
+                        <div class="grid">
+                            {#if 'attributes' in value.data}
+                                <svelte:self data={value.data.attributes} />
+                            {:else}
+                                {#each value.data as a1}
+                                    {#if 'attributes' in a1}
+                                        <svelte:self data={a1.attributes} />
+                                    {/if}
+                                {/each}
+                            {/if}
+                        </div>
+                    </div>
+                </button>
+            {:else if 'qty' in value}
+                {value.type + '\tType: ' + value.type}
+            {:else if 'dose' in value}
+                {value.dose + ' ' + value.units}
+            {:else if 'temperature' in value}
+                {value.temperature + ' ' + value.type}
+            {/if}
         </div>
     {:else}
         <div class="item">{value}</div>
